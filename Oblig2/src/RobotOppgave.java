c61df2fc2fd1fd22e22db33b8e7ec594c8b3647f
Viusan <viusansantos@gmail.com>
2024-09-24 20:02:16 +0200

    oblig2 finished i think

--- Oblig2/src/RobotOppgave.java
+++ Oblig2/src/RobotOppgave.java
@@ -5,37 +5,57 @@ class Verden {
 
     // endre metoden for å returnere true hvis det er søndag (hint: Modulo %)
     public boolean erSondag() {
+        if (dag % 7 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean erMandag() {
+        if (dag % 8 == 0){
+            return true;
+        }
         return false;
     }
 }
 
 class Robot {
     // gi robotten 4 attributter: navn, batteriNivaa, avstandTilParken og botType
+    String name;
+    Double batteryLevel;
+    int distanceToPark;
+    String botType;
 
     // ENdre metoden slik at den retunerer en tekst streng som forklarer statusen til roboten
     // eksempel: Dette er bot Dancatron 4000 av type B-Bot.
     // Denne enheten har 80.0 batterikapasitet igjen og bor 1500 meter fra parken.
     public String giStatus(){
-        return "";
+        return "Dette er en bot " + name + " av type " + botType + ". Denne enheten har " + batteryLevel + " batterikapasitet igjen og bor " + distanceToPark + " meter fra parken";
     }
 
     // Metode for å sjekke om roboten kan gå til parken basert på omgivelsene
     public boolean gaaTilParken(Verden verden) {
-//        System.out.println(navn + " sjekker om det er mulig å gå til parken...");
+        System.out.println(name + " sjekker om det er mulig å gå til parken...");
 
         // Sjekk om det regner
         if (verden.regner){
-            // skriv ut hvorfor boten ikke kan gå i parken med System.out.println
+            System.out.println("Roboten kan ikke gå ut siden det regner.");
             return false;
         }
 
-
         // Sjekk om det er søndag. Kan bare gå i parken på søndager.
-
+        if(verden.erSondag()){
+            System.out.println("Det er søndag så den kan gå i parken.");
+        }else{
+            System.out.println("Du er ikke søndag idag, du kan ikke gå i parken.");
+        }
 
         // Sjekk batterinivå. En bot trenger 1% batterinivå per 100 meter for å gå til parken.
-
-
+        if(batteryLevel * 100 < distanceToPark){
+            System.out.println(name + " har ikke nok strøm for å komme seg til parken.");
+        }else{
+            System.out.println(name + " har nok strøm for å komme seg til parken." );
+        }
         return true;
     }
 
@@ -44,6 +64,23 @@ class Robot {
     // En bot trenger minimum 50% batterikapasitet for å være med
     // Det kan ikke være mandag. Da er danseklubben stengt.
 
+    public boolean gaaTilDanseklubb(Verden verden, Robot bot) {
+        if (verden.erMandag()) {
+            System.out.println("Det er mandag idag, desverre er danseklubben stengt.");
+        } else {
+            if (bot.botType.equals("B-Bot")) {
+                System.out.println("Du er av riktig type robott ;)");
+                if (bot.batteryLevel >= 50) {
+                    System.out.println("Ser ut som at du har nok strøm for å komme deg inni klubben også.");
+                } else {
+                    System.out.println("Batterikapasitetet ditt må minimum være 50%, du ligger på " + batteryLevel + ". Gå hjem og lad degselv.");
+                }
+            } else {
+                System.out.println("Du er ike av riktig type robott >:(");
+            }
+        }
+        return true;
+    }
 }
 
 public class RobotOppgave {
@@ -51,8 +88,19 @@ public class RobotOppgave {
         // Oppretter en Verden-objekt (omgivelsene)
         Verden dagensVerden = new Verden();
         dagensVerden.regner = false;
-        dagensVerden.dag = 122;
+        dagensVerden.dag = 8;
 
+        Robot Spark = new Robot();
+        Spark.name = "Spark-E";
+        Spark.batteryLevel = 85.0;
+        Spark.distanceToPark = 2000;
+        Spark.botType = "A-Bot";
+
+        Robot Shark = new Robot();
+        Shark.name = "Bark-B";
+        Shark.batteryLevel = 25.0;
+        Shark.distanceToPark = 3340;
+        Shark.botType = "B-Bot";
         // Oppretter to Robot-objekter
         // med navn, f.eks. Dancatron 4000 og Spark-E
         // med botType "B-Bot" og "Toaster"
@@ -62,5 +110,15 @@ public class RobotOppgave {
 
         // Sjekker om robotene kan gå til danseklubben
         // Sjekker om robotene kan gå til parken
+        String status = Spark.giStatus();
+        System.out.println(status);
+        Spark.gaaTilParken(dagensVerden);
+        Spark.gaaTilDanseklubb(dagensVerden, Spark);
+
+        System.out.println("--------------------");
+        String statusTwo = Shark.giStatus();
+        System.out.println(statusTwo);
+        Shark.gaaTilParken(dagensVerden);
+        Shark.gaaTilDanseklubb(dagensVerden, Shark);
     }
 }
